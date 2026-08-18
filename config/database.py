39fc738a0501eaed63be4ba9743d5ad13ca39aae
João Francisco de Sá Mourão Amaral <joaoamaral1706@gmail.com>
2026-08-18 19:11:43 +0100

    Modify MongoDB connection string for security
    Updated MongoDB connection string to include placeholders for user and password.

--- config/database.py
+++ config/database.py
@@ -4,7 +4,9 @@ from pymongo import MongoClient
 
 # Estabelece a ligação (o "cliente") à base de dados alojada na cloud (MongoDB Atlas). 
 # O texto entre aspas é a "Connection String", que contém o utilizador, a password e o endereço do servidor.
-client = MongoClient("mongodb+srv://joaoamaral1706_db_user:sgekjckPWERSzYNa@api-rest.jxef0hx.mongodb.net/?appName=API-REST")
+
+# Alterar o user e a password da base de dados para o seu acesso/autenticação!!!
+client = MongoClient("mongodb+srv://nome-do-utilizador:password")
 
 # Seleciona a base de dados específica chamada 'product_db' a partir da ligação que acabámos de criar. 
 # (Se a base de dados não existir, o MongoDB cria-a automaticamente quando inserires o primeiro dado).
